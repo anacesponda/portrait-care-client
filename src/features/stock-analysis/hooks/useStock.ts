eec5e40cca6066503f9f0c592c023e43175e75a2
Ana Clara Esponda <anacesponda@gmail.com>
2024-06-14 12:28:52 -0300

    Fix types

--- src/features/stock-analysis/hooks/useStock.ts
+++ src/features/stock-analysis/hooks/useStock.ts
@@ -1,10 +1,11 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useAppDispatch } from './useDispatch';
 import { RootState } from '../../../stores/store';
 import { fetchStock } from '../stores/stockSlice';
 
 export const useStock = (ticker: string) => {
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
   const stockData = useSelector((state: RootState) => state.stock.data);
   const loading = useSelector((state: RootState) => state.stock.loading);
   const error = useSelector((state: RootState) => state.stock.error);
